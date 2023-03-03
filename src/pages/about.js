515051218bcd345ecb5af59e64584a53538443aa
mukai-junichi <mju.6000@gmail.com>
2023-03-03 16:34:50 +0900

    ページ作成

---
+++ src/pages/about.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Junichi Mukai's Portfolio</title>
+        <meta name="description" content="Junichi Mukai's Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      <Main
+        title="About me"
+      />
+      <Footer />
+    </>
+  );
+}
