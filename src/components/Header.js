e361355f6ce779e62760b3ee749eb44d90ffffeb
mukai-junichi <mju.6000@gmail.com>
2023-03-03 18:03:31 +0900

    tailcssの導入

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,39 @@ import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <div>
-        <Link href="/">Junichi Mukai</Link>
+    <header className="border-b border-gray-200">
+      <div className="container flex flex-col md:flex-row items-center mx-auto p-5">
+        <Link href="/" className="font-medium mb-4 md:mb-0">
+          <span className="text-xl ml-3">Junichi Mukai</span>
+        </Link>
+        <nav className="md:ml-auto text-base">
+          <ul className="flex">
+            <li className="mr-5">
+              <Link href="/" className=" hover:text-blue-400 duration-300">
+                Home
+              </Link>
+            </li>
+            <li className="mr-5">
+              <Link href="/about" className=" hover:text-blue-400 duration-300">
+                About
+              </Link>
+            </li>
+            <li className="mr-5">
+              <Link href="/works" className=" hover:text-blue-400 duration-300">
+                Works
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/skills"
+                className=" hover:text-blue-400 duration-300"
+              >
+                Skills
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-
-          <li>
-            <Link href="/works">Works</Link>
-          </li>
-
-          <li>
-            <Link href="/skills">Skills</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
