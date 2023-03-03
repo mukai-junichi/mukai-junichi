23584c6fb4c26b1fa51c38d0c45506bc5ee65ea4
mukai-junichi <mju.6000@gmail.com>
2023-03-04 00:01:48 +0900

    footer作成

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 
+const today = new Date();
+const year = today.getFullYear();
+
 const Footer = () => {
   return(
-    <footer>
-      footer
+    <footer className="border-t border-gray-200">
+      <div className="container mx-auto flex flex-col justify-center items-center p-5 md:flex-row">
+        <span>©{year} Junichi Mukai.</span>
+      </div>
     </footer>
   );
 }
