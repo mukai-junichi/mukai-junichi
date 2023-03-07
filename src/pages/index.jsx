60a23fee569b3086c3900c0ef6636260d1964cad
mukai-junichi <mju.6000@gmail.com>
2023-03-07 13:54:14 +0900

    Home画面の微修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,20 +20,17 @@ export default function Home() {
       <Header />
       <Main>
         <>
-          <div className="mt-6 flex flex-col items-center justify-center md:mt-20 md:flex-row">
+          <div className="mt-6 md:mt-14 flex flex-col md:flex-row items-center justify-center ">
             <div className="flex flex-col px-2 md:mr-24 lg:max-w-lg">
               <h1 className="mb-4 text-center text-3xl font-medium text-gray-900 sm:text-5xl md:text-left">
-                I'm
+                I'm Junichi Mukai
                 <div className="my-4 flex flex-col justify-center items-center md:mt-8">
                   <span className="w-fit py-2 bg-gradient-to-r from-violet-800 via-blue-500 to-indigo-600 bg-clip-text text-transparent">Web Developer</span>
                   <span className="text-center">&</span>
                   <span className="w-fit py-2 bg-gradient-to-r from-violet-800 via-blue-500 to-indigo-600 bg-clip-text text-transparent">Web Designer</span>
                 </div>
               </h1>
-              <p className="mb-8 text-center leading-relaxed md:text-left">
-                ご覧いただきありがとうございます！<br></br>向井純一と申します。
-              </p>
-              <button className="mx-auto mb-8 w-fit rounded border-0 bg-green-500 py-2 px-6 text-lg text-white duration-300 hover:bg-green-600 md:mb-0">
+              <button className="mx-auto my-0 md:my-8 mb-8 md:mb-0 w-fit rounded border-0 bg-green-500 py-2 px-6 text-lg text-white duration-300 hover:bg-green-600">
                 Contact
               </button>
             </div>
@@ -45,6 +42,8 @@ export default function Home() {
                 alt=""
               />
               <p className="mt-2 leading-relaxed">
+                名前：向井 純一
+                <br></br>
                 年齢：
                 <MyAge {...MyBirthday} />歳<br></br>
                 住所：神奈川県川崎市
