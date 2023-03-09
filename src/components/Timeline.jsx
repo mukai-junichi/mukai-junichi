b23489103abd7dc4d0ecf2d53058ee125de16dfc
mukai-junichi <mju.6000@gmail.com>
2023-03-09 22:14:27 +0900

    Timeline修正：改行コード\n入り文字列を、JSXで</ br>に変換する処理

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -17,14 +17,14 @@ const MyTimeline = [
         data: "23歳〜25歳（2008年 - 2011年）",
         title: "【営業・新規事業立ち上げ】知人が会社を設立し、誘いを受け転職",
         history:
-          "ホームセキュリティ機器販売の営業職として従事し、兼任して新規事業の立ち上げも担当。<br></br>24歳の時に新しく支店を開設し、福岡へ転勤。<br></br>25歳の時に会社が倒産。",
+          "ホームセキュリティ機器販売の営業職として従事し、兼任して新規事業の立ち上げも担当。\n24歳の時に新しく支店を開設し、福岡へ転勤。\n25歳の時に会社が倒産。",
       },
       {
         key: Math.floor(Math.random() * 1e3),
         data: "25歳〜30歳（2011年 - 2015年）",
         title: "【起業・経営】前職の同僚と会社を設立",
         history:
-          "知人にコンサルティングを受け、ポイントサイトメディアを立ち上げ運営を行う。<br></br>アクセス集計・分析、企画・運営、サイトのバナー制作、経理・事務業務等、経営を経験し必要知識を習得。<br></br>運営を続けるも業績が安定せず、30歳の時に会社員としての転職を決意。",
+          "知人にコンサルティングを受け、ポイントサイトメディアを立ち上げ運営を行う。\nアクセス集計・分析、企画・運営、サイトのバナー制作、経理・事務業務等、経営を経験し必要知識を習得。\n運営を続けるも業績が安定せず、30歳の時に会社員としての転職を決意。",
       },
     ],
   },
@@ -37,14 +37,14 @@ const MyTimeline = [
         data: "30歳〜35歳（2015年 - 2020年）",
         title: "【営業・部門管理】会社員として転職",
         history:
-          "医療・福祉業界特化型の人材派遣・紹介業及び教育事業を行なっている会社に入社。<br></br>東京本社の福祉人材部門に配属となり営業職として従事。<br></br>その後、部門管理・運営、新規事業の考案・既存事業のビジネスモデルの見直し、社内の基盤システムの構築等にも携わる。<br></br>業務の中で課題解決のためにシステムが必要不可欠で、それを自身で作れる人材になりたいと強く思うようになり、エンジニア転職へのチャレンジを決意。",
+          "医療・福祉業界特化型の人材派遣・紹介業及び教育事業を行なっている会社に入社。\n東京本社の福祉人材部門に配属となり営業職として従事。\nその後、部門管理・運営、新規事業の考案・既存事業のビジネスモデルの見直し、社内の基盤システムの構築等にも携わる。\n業務の中で課題解決のためにシステムが必要不可欠で、それを自身で作れる人材になりたいと強く思うようになり、エンジニア転職へのチャレンジを決意。",
       },
       {
         key: Math.floor(Math.random() * 1e3),
         data: "2020年10月~2020年12月",
         title: "【学習】短期プログラミングスクールで学習",
         history:
-          "退職し、エンジニア転職するためにプログラミングスクールで学習を開始。<br></br>約3ヶ月間の短期集中型でHTML・CSS・JavaScript・Ruby（Ruby on Rails）等を学習。",
+          "退職し、エンジニア転職するためにプログラミングスクールで学習を開始。\n約3ヶ月間の短期集中型でHTML・CSS・JavaScript・Ruby（Ruby on Rails）等を学習。",
       },
       {
         key: Math.floor(Math.random() * 1e3),
@@ -52,14 +52,14 @@ const MyTimeline = [
         title:
           "【エンジニア】スクールでの学習を終えバックエンドエンジニアとして転職（フロントも一部対応）",
         history:
-          "海外ユーザ向けの日本のアニメ情報サイトの開発、保守運営業務に従事。<br></br>■ 開発言語<br></br>PHP/Symfony, SQL/MySQL, HTML/CSS/SASS, Javascript/jQuery<br></br>■インフラ・その他<br></br>AWS, GA4/GTM, GitHub, Slack, MacPC",
+          "海外ユーザ向けの日本のアニメ情報サイトの開発、保守運営業務に従事。\n■ 開発言語\nPHP/Symfony, SQL/MySQL, HTML/CSS/SASS, Javascript/jQuery\n■インフラ・その他\nAWS, GA4/GTM, GitHub, Slack, MacPC",
       },
       {
         key: Math.floor(Math.random() * 1e3),
         data: "37歳~（2022年 - 現在）",
         title: "【エンジニア兼PM】ステップアップのため転職",
         history:
-          "エンジニアとしての開発スキルを高めたく、ステップアップ転職。<br></br>教育機関や一般企業向けに、eラーニングプラットフォームの構築、保守運用のサービス提供している会社にてPMとして従事。",
+          "エンジニアとしての開発スキルを高めたく、ステップアップ転職。\n教育機関や一般企業向けに、eラーニングプラットフォームの構築、保守運用のサービス提供している会社にてPMとして従事。",
       },
     ],
   },
@@ -76,10 +76,21 @@ const Timeline = () => {
               {Content.episodes.map((Episode) => {
                 return (
                   <li key={Episode.key} className={styles.timeline__episodes}>
-                    <p className={styles.timeline__data}>{Episode.data}</p>
-                    <p className={styles.timeline__title}>{Episode.title}</p>
+                    <p className={styles.timeline__data}>
+                      {Episode.data}
+                    </p>
+                    <p className={styles.timeline__title}>
+                      {Episode.title}
+                    </p>
                     <p className={styles.timeline__description}>
-                      {Episode.history}
+                      {Episode.history.split('\n').map((text, index) => {
+                        return (
+                          <Fragment key={index}>
+                            {text}
+                            <br />
+                          </Fragment>
+                        )
+                      })}
                     </p>
                   </li>
                 );
